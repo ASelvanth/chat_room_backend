45f0402a373981bff2df3d4669726e8e2b896231
ASelvanth <ajselva04@gmail.com>
2023-05-08 15:53:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const http = require('http'); //default http package
+const { Server } = require('socket.io');
+
+const app = express();
+
+//ex for http socket connection
+const server = http.createServer(app);
+
+//socket connection
+const io = new Server(server,{
+    cors: {
+        origin :'http://localhost:3000',
+        methods : ['GET', 'POST']
+    }
+});
+
+app.get('/',(req,res)=>{
+    res.send("Chat App Home Page");
+});
+
+io.on('connection', (socket) => {
+
+    socket.on('join-room', (data) => {
+        socket.join(data);
+        console.log(`user ${socket.id} has joined the room ${data}`);
+    });
+
+    socket.on('send-message', (data) => {
+        console.log('Data: ', data );
+        socket.to(data.room).emit('receive-message', data);
+    } );
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected: ', socket.id);
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`App is running on PORT ${PORT}`);
+})
